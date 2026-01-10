7dd452389bb4cf2ccfa53f2f49b173a4963af6b7
nowatanabe <101636935+nowatanabe@users.noreply.github.com>
2026-01-10 04:49:55 +0000

    add session handling

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,20 +11,31 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.service.HomeService;
 
-@Controller
-public class HomeController {
+import jakarta.servlet.http.HttpSession;
 
+@Controller
+public class HomeController
+{
     private HomeService service;
 
-    public HomeController(HomeService homeService) {
+    public HomeController(HttpSession session, HomeService homeService) {
         this.service = homeService;
     }
 
     @RequestMapping(value="/", method=RequestMethod.GET)
-    public ModelAndView index(ModelAndView mav) {
-        mav.setViewName("index");
+    public ModelAndView index(HttpSession session, ModelAndView mav) {
+        session.setAttribute("hoge", "ほげ");
+
+        List<String> sessionInfo = new ArrayList<String>(); 
+        session.getAttributeNames().asIterator().forEachRemaining(name -> {
+            Object value = session.getAttribute(name);
+            sessionInfo.add(String.format("['%s', %s]", name, value.toString()));
+        });
 
         mav.addObject("characters", service.getAllCharacters());
+        mav.addObject("sessions", sessionInfo);
+
+        mav.setViewName("index");
         return mav;
     }
 }
